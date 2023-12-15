59ed6616c09d8ea3e5b7ad4f320fbfd7b87ee051
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2023-12-14 20:55:08 -0500

    updates to performance

--- DictionaryFunctions.cs
+++ DictionaryFunctions.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Security.Cryptography;
 using System.Text.Json;
 using cloud_dictionary.Shared;
 using Microsoft.Azure.Functions.Worker;
@@ -94,7 +95,6 @@ namespace cloud_dictionary
         public async Task<HttpResponseData> GetRandomDefinition(
             [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "application/json; charset=utf-8");
@@ -139,6 +139,29 @@ namespace cloud_dictionary
                 return response;
             }
 
+        }
+
+        [Function("GetDefinitionOfTheDay")]
+        public async Task<HttpResponseData> GetDefinitionOfTheDay([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            var definition = await _dictionaryRepository.GetDefinitionOfTheDay();
+            response.WriteString(JsonSerializer.Serialize(definition));
+
+            return response;
+
+        }
+        
+        [Function("UpdateDefinitionOfTheDay")]
+        public async Task Run([TimerTrigger("0 0 0 * * *", RunOnStartup = true)] TimerInfo myTimer)
+        {
+
+            // Logic to select a random definition
+            var definition = await _dictionaryRepository.GetRandomDefinitionAsync();
+
+            // Store the selected definition as 'Definition of the Day'
+            await _dictionaryRepository.UpdateDefinitionOfTheDay(definition);
         }
 
     }
--- DictionaryRepository.cs
+++ DictionaryRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using cloud_dictionary.Shared;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Linq;
@@ -9,11 +10,14 @@ namespace cloud_dictionary
     {
         private readonly Container _definitionsCollection;
         private readonly Container _counterCollection;
+        private readonly Container _definitionOfTheDayCollection;
+        private static readonly Random random = new();
         public DictionaryRepository(CosmosClient client, IConfiguration configuration)
         {
             var database = client.GetDatabase(configuration["AZURE_COSMOS_DATABASE_NAME"]);
             _definitionsCollection = database.GetContainer(configuration["AZURE_COSMOS_CONTAINER_NAME"]);
             _counterCollection = database.GetContainer(configuration["AZURE_COSMOS_COUNTER_CONTAINER_NAME"]);
+            _definitionOfTheDayCollection = database.GetContainer(configuration["AZURE_COSMOS_DEFINITION_OF_THE_DAY_CONTAINER_NAME"]);
         }
         public async Task<IEnumerable<Definition>> GetDefinitionsAsync(int? skip, int? batchSize)
         {
@@ -100,7 +104,6 @@ namespace cloud_dictionary
             }
 
         }
-
         public async Task DecreaseCountAsync()
         {
             var countDocument = await GetCountDocumentAsync();
@@ -135,8 +138,7 @@ namespace cloud_dictionary
         {
 
             int count = await GetDefinitionCountAsync();
-
-            int randomIndex = new Random().Next(0, count);
+            int randomIndex = random.Next(0, count);
             // Query to get the random document
             var query = _definitionsCollection.GetItemLinqQueryable<Definition>()
                 .Skip(randomIndex)
@@ -198,11 +200,52 @@ namespace cloud_dictionary
         public async Task<int> GetDefinitionCountAsync()
         {
 
-
             var countDocument = await _counterCollection.ReadItemAsync<Counter>("counterId", new PartitionKey("counterId"));
 
             return countDocument.Resource.Count;
         }
+
+        public async Task<Definition?> GetDefinitionOfTheDay()
+        {
+            var query = _definitionOfTheDayCollection.GetItemLinqQueryable<Definition>().Take(1).ToFeedIterator();
+
+            if (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+                return response.FirstOrDefault();
+            }
+
+            return null;
+
+        }
+
+   
+        public async Task UpdateDefinitionOfTheDay(Definition newDefinition)
+        {
+            // Fetch the current 'Definition of the Day', if it exists
+            var query = _definitionOfTheDayCollection.GetItemLinqQueryable<Definition>().Take(1).ToFeedIterator();
+            Definition currentDefinition = null;
+            if (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+                currentDefinition = response.FirstOrDefault();
+            }
+
+            // Delete the current definition if it exists
+            if (currentDefinition != null)
+            {
+                await _definitionOfTheDayCollection.DeleteItemAsync<Definition>(currentDefinition.Id, new PartitionKey(currentDefinition.Id));
+            }
+
+            // Add the new 'Definition of the Day'
+            await _definitionOfTheDayCollection.UpsertItemAsync(newDefinition, new PartitionKey(newDefinition.Id));
+        }
+
+
+
+
+
+
 
     }
 }
